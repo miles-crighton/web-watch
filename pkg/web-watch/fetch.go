daaac37b8e5bc6efe2654f0a42534e83bfbe0794
Miles Crighton <miles.crighton@gmail.com>
2024-04-14 17:32:43 +0100

    Store fetched page as file

--- pkg/web-watch/fetch.go
+++ pkg/web-watch/fetch.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func Fetch() {
 	resp, err := http.Get("http://google.com")
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
+	check(err)
 	fmt.Printf("Response body: %v", string(body))
+
+	err = os.MkdirAll("page-store", 0750)
+	check(err)
+	file, err := os.Create("page-store/google.com")
+	check(err)
+	defer file.Close()
+
+	file.Write(body)
+
+	fmt.Println("Succesfully saved file")
 }
