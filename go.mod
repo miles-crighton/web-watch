14d70a3590d29e5ec6486ceb021e6d92cba86ed9
Miles Crighton <miles.crighton@gmail.com>
2024-04-21 11:51:02 +0100

    Add basic HTML parsing and printing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-watch
 
 go 1.21.4
+
+require golang.org/x/net v0.24.0 // indirect
--- pkg/web-watch/fetch.go
+++ pkg/web-watch/fetch.go
@@ -2,9 +2,11 @@ package webwatch
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -13,15 +15,35 @@ func check(e error) {
 	}
 }
 
+func traverseNode(n *html.Node, depth int) {
+	indent := strings.Repeat("  ", depth)
+
+	switch n.Type {
+	case html.ElementNode:
+		fmt.Printf("%s<%s>\n", indent, n.Data)
+	case html.TextNode:
+		text := strings.TrimSpace(n.Data)
+		if text != "" {
+			fmt.Printf("%s%s\n", indent, n.Data)
+		}
+	case html.CommentNode:
+		fmt.Printf("%s<!-- %s -->\n", indent, n.Data)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseNode(c, depth+1)
+	}
+}
+
 func Fetch() {
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("https://google.com")
 	check(err)
 
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	// body, err := io.ReadAll(resp.Body)
 	check(err)
-	fmt.Printf("Response body: %v", string(body))
+	// fmt.Printf("Response body: %v", string(body))
 
 	err = os.MkdirAll("page-store", 0750)
 	check(err)
@@ -29,7 +51,11 @@ func Fetch() {
 	check(err)
 	defer file.Close()
 
-	file.Write(body)
+	// file.Write(body)
 
 	fmt.Println("Succesfully saved file")
+
+	doc, err := html.Parse(io.Reader(resp.Body))
+	check(err)
+	traverseNode(doc, 0)
 }
